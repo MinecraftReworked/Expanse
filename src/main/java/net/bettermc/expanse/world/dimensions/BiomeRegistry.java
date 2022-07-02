6410e71b6104005634c20c9b116456ac559ee393
Colin Vaughn <colinvaughn01@gmail.com>
2022-07-02 17:19:59 -0400

    Mars generation is now setup.

--- src/main/java/net/bettermc/expanse/world/dimensions/BiomeRegistry.java
+++ src/main/java/net/bettermc/expanse/world/dimensions/BiomeRegistry.java
@@ -11,4 +11,11 @@ public class BiomeRegistry {
             new Identifier(ExpanseMain.MOD_ID, "moon_desert"));
     private static final RegistryKey<Biome> ORBIT = RegistryKey.of(Registry.BIOME_KEY,
             new Identifier(ExpanseMain.MOD_ID, "orbit"));
+
+    private static final RegistryKey<Biome> MARS_DESERT = RegistryKey.of(Registry.BIOME_KEY,
+            new Identifier(ExpanseMain.MOD_ID, "mars_desert"));
+    private static final RegistryKey<Biome> MARS_ICE_SPIKES = RegistryKey.of(Registry.BIOME_KEY,
+            new Identifier(ExpanseMain.MOD_ID, "mars_ice_spikes"));
+    private static final RegistryKey<Biome> MARS_ROCKY_PLAINS = RegistryKey.of(Registry.BIOME_KEY,
+            new Identifier(ExpanseMain.MOD_ID, "mars_rocky_plains"));
 }
